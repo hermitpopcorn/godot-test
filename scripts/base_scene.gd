8830a1c7e94ff186c50ea4e5a40726e7d296e9e1
Dani Nugraha <dani.hmng@gmail.com>
2020-04-08 13:31:45 +0700

    add next arrow and sfx to message box

---
+++ scripts/base_scene.gd
@@ -0,0 +1,14 @@
+extends Node
+
+class_name BaseScene
+
+func _input(event):
+	if (!$PausedIndicator.visible):
+		if event.is_action_pressed("ui_cancel"):
+			get_tree().paused = true
+			$PausedIndicator.visible = true
+
+func center_window():
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
